6d561cf3e2975d7d9ace8af62b634c51a63c1d4c
Leejaeyeop <dlwoduq78@naver.com>
2025-06-05 13:52:52 +0900

    <chore>: move-components
    - move-components

--- src/app/(pixijs)/App.tsx
+++ src/app/(pixijs)/App.tsx
@@ -2,26 +2,22 @@
 
 import { Application, ApplicationRef, extend } from "@pixi/react";
 import { Container, Graphics, Sprite, AnimatedSprite } from "pixi.js";
-import { BackgroundSprite } from "./components/sprites/BackgroundSprite";
 import { useEffect, useRef } from "react";
 import { MAX_SIZE } from "./constants/sizes";
-import { VisitorSprite } from "./components/sprites/VisitorSprite";
 import { useRoundManager } from "./hooks/use-round-manager";
 import { useResize } from "./hooks/use-resize";
 // extend tells @pixi/react what Pixi.js components are available
 import { RoundProvider } from "./hooks/use-round-manager";
-import { StageProvider, useStageManager } from "./hooks/use-stage-manager";
-
-import { useShallow } from "zustand/shallow";
+import { StageProvider } from "./hooks/use-stage-manager";
 
 import { useMenuStore } from "@/store/useMenuStore";
 
 import { MainMenu } from "@/components/overlay/menu/MainMenu";
 import { SCENE_IDS, useAppStore } from "@/store/useAppStore";
-import MonacoEditor from "@/components/overlay/MonacoEditor";
-import { ActionBar } from "@/components/overlay/actionBar/ActionBar";
-import { TopHUD } from "@/components/overlay/hud/TopHUD";
-import { RoundAnswerResult } from "@/components/overlay/result/RoundAnswerResult";
+
+import HTMLOverlay from "@/components/overlay/HTMLOverlay";
+import { BackgroundSprite } from "@/components/canvas/sprites/BackgroundSprite";
+import { VisitorSprite } from "@/components/canvas/sprites/VisitorSprite";
 
 extend({
   Graphics,
@@ -54,45 +50,6 @@ export default function AppContainer() {
         </RoundProvider>
       </StageProvider>
     </div>
-  );
-}
-
-function HTMLOverlay() {
-  const { curRoundQuiz, isVisibleTopHUD, isPaused } = useStageManager();
-
-  const { isVisibleAnswer, isVisibleActionBar, isVisibleRoundResult } =
-    useRoundManager();
-
-  const [isMenuOpen, menuOverlay] = useMenuStore(
-    useShallow((state) => [state.isMenuOpen, state.menuOverlay])
-  );
-  return (
-    <>
-      {/* top HUD */}
-      {isVisibleTopHUD && <TopHUD />}
-      {/* question */}
-      <div
-        className={`absolute bottom-[45%] h-1/5 w-1/2 left-1/4 ${
-          !isVisibleAnswer && "hidden"
-        }`}
-      >
-        <div className="relative inline-block bg-white h-full w-full px-2 py-2 rounded-xl shadow-md">
-          {/* {curRoundQuiz?.answer} */}
-          <MonacoEditor value={curRoundQuiz?.question} />
-          <div className="absolute left-1/2 bottom-[-8px] w-0 h-0 border-x-8 border-x-transparent border-t-8 border-t-white"></div>
-        </div>
-      </div>
-      {/* action bar */}
-      {isVisibleActionBar && <ActionBar />}
-      {/* menu */}
-      {isMenuOpen && menuOverlay}
-      {/* pause */}
-      {isPaused && (
-        <div className="absolute top-0 w-full h-full backdrop-blur-md" />
-      )}
-      {/* answer result */}
-      {isVisibleRoundResult && <RoundAnswerResult />}
-    </>
   );
 }
 
