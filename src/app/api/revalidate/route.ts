4109d453b4e73d55a394fd8c9fb8a6c7b95b5dc3
jaeyeop <93045720+Leejaeyeop@users.noreply.github.com>
2025-06-06 14:59:01 +0900

    Ci/GitHub actions (#11)
    * <ci>: prepare ci/cd test
    
    - add prod scripts
    - add schema
    - change revalidate rotue for secure
    - add eslint ignoreDuringBuilds
    
    * <test>: init ci/cd test
    
    * <test>: change diff bracnh to test branch
    
    * <test>: add test.json file to src
    
    * <test>: change diff logic
    
    * <test>: change diff logic # 2
    
    * <test>: change src directory
    
    * <test>: add pnpm
    
    * <test>: add setup-node
    
    * <test>:  Install pnpm explicitly
    
    * <chore>: change prod scipt for deploy
    
    * <test>:  change quizz data & stage error handling
    
    * <ci>:  set push branch to main

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -4,15 +4,21 @@ import { NextRequest, NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
 
 export async function POST(request: NextRequest) {
-  const secret = request.nextUrl.searchParams.get("secret");
+  const revalidationSecret = request.headers.get("x-revalidate-secret");
 
-  if (secret !== process.env.REVALIDATION_SECRET) {
+  if (revalidationSecret !== process.env.REVALIDATION_SECRET) {
     return NextResponse.json(
       { message: "Invalid secret token" },
       { status: 401 }
     );
   }
 
-  revalidatePath("/");
-  return NextResponse.json({ revalidated: true, now: Date.now() });
+  try {
+    // 원하는 경로를 재검증합니다. (예: 모든 경로를 재검증하려면 '/')
+    revalidatePath("/");
+    return NextResponse.json({ revalidated: true, now: Date.now() });
+  } catch (err) {
+    // 오류 발생 시
+    return NextResponse.json("Error revalidating: " + err, { status: 500 });
+  }
 }
